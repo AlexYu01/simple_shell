59d1e78ff5c0674721f97bf17d4ccaca138a73e3
AlexYu01 <tengalexyu@gmail.com>
2018-11-19 23:38:27 +0000

    Implement function to string together syntax errors

--- shell.h
+++ shell.h
@@ -115,6 +115,7 @@ char *error_env(char **args);
 char *error_1(char **args);
 char *error_2_exit(char **args);
 char *error_2_cd(char **args);
+char *error_2_syntax(char **args);
 char *error_126(char **args);
 char *error_127(char **args);
 

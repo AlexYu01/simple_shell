b95229d94bd093719b84fd52d443e3b48cf10ad1
AlexYu01 <tengalexyu@gmail.com>
2018-11-21 05:22:14 +0000

    Place \0 in the buffer after calling read

--- proc_file_comm.c
+++ proc_file_comm.c
@@ -67,33 +67,28 @@ int proc_file_commands(char *file_path, int *exe_ret)
 		*exe_ret = cant_open(file_path);
 		return (*exe_ret);
 	}
-	b_read = read(file, buffer, 120);
-	if (b_read == 0)
-		return (*exe_ret);
 	line = malloc(sizeof(char) * old_size);
 	if (!line)
 		return (-1);
-	line[0] = '\0';
-
-	while (b_read)
-	{
+	do {
+		b_read = read(file, buffer, 119);
+		if (b_read == 0 && line_size == 0)
+			return (*exe_ret);
+		buffer[b_read] = '\0';
 		line_size += b_read;
 		line = _realloc(line, old_size, line_size);
 		_strcat(line, buffer);
 		old_size = line_size;
-		b_read = read(file, buffer, 120);
-	}
-	for (i = 0; i < line_size; i++)
+	} while (b_read);
+	for (i = 0; line[i] == '\n'; i++)
+		line[i] = ' ';
+	for (; i < line_size; i++)
 	{
 		if (line[i] == '\n')
 		{
-			if (line[i + 1] == '\n')
-			{
+			line[i] = ';';
+			for (i += 1; i < line_size && line[i] == '\n'; i++)
 				line[i] = ' ';
-				line[i + 1] = ' ';
-			}
-			else
-				line[i] = ';';
 		}
 	}
 	variable_replacement(&line, exe_ret);
